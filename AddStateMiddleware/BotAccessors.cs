672e50eb4517ee711b8bdb65b8fc0536f3d8a0fa
danieldare <dodanieloluwadare@gmail.com>
2018-10-30 18:19:45 +0000

    added state middleware project

---
+++ AddStateMiddleware/BotAccessors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Bot.Builder;
+
+namespace AddStateMiddleware
+{
+    public class BotAccessors
+    {
+        public BotAccessors(ConversationState conversationState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+        }
+
+        public static string DemoStateName { get; } = $"{nameof(BotAccessors)}.DemoStateAccessor";
+
+        public IStatePropertyAccessor<DemoState> DemoStateAccessor { get; set; }
+        public ConversationState ConversationState { get; }
+    }
+}
---
+++ AddStateMiddleware/ConversationState.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace AddStateMiddleware
+{
+    internal class ConversationState<T> : IMiddleware
+    {
+        private MemoryStorage storage;
+
+        public ConversationState(MemoryStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
