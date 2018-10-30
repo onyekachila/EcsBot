12ec18dd3742576320f3eb3e354754688ab36582
danieldare <dodanieloluwadare@gmail.com>
2018-10-30 16:49:07 +0000

    Add project files.

---
+++ BotMiddleware/Middleware/SimpleMiddlewareOne.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BotMiddleware.Middleware
+{
+    public class SimpleMiddlewareOne : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await turnContext.SendActivityAsync($"[SimpleMiddlewareOne] {turnContext.Activity.Type}/OnTurn/Before");
+
+            await next(cancellationToken);
+
+            await turnContext.SendActivityAsync($"[SimpleMiddlewareOne] {turnContext.Activity.Type}/OnTurn/After");
+        }
+    }
+}
---
+++ BotMiddleware/Middleware/SimpleMiddlewareTwo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BotMiddleware.Middleware
+{
+    public class SimpleMiddlewareTwo : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await turnContext.SendActivityAsync($"[SimpleMiddlewareTwo] {turnContext.Activity.Type}/OnTurn/Before");
+
+            if (turnContext.Activity.Type == ActivityTypes.Message && turnContext.Activity.Text == "secret password")
+            {
+                // calling next() is totally optional. if the middleware does not call next then the
+                // next middleware in the pipeline will not be called, AND the bot will not receive the message.
+                //
+                // in this instance, we are only handing the message to downstream bots if the user says "secret password"
+                await next(cancellationToken);
+            }
+
+            await turnContext.SendActivityAsync($"[SimpleMiddlewareTwo] {turnContext.Activity.Type}/OnTurn/After");
+        }
+    }
+}
